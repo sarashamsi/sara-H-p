c013bab74d5c65591fcbaaf7d371017f5d647dc8
sara <sarashmsi834@gmail.com>
2025-04-03 10:34:54 +0330

    complete entity and database class

---
+++ src/db/Database.java
@@ -0,0 +1,43 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>() ;
+    private static int nextID = 1 ;
+
+    // برای جلوگیری از ایجاد نمونه کلاس Database
+    private Database() {}
+
+    public static void add(Entity e) {
+        Entity copy = e.copy();
+        copy.id = nextID ++ ;
+        entities.add(copy) ;
+        // update ID
+        e.id = copy.id ;
+
+    }
+
+    public static Entity get( int id ) throws EntityNotFoundException {
+        for (Entity e : entities) {
+            if (e.id == id){
+                return e.copy() ;
+            }
+        }
+        throw new EntityNotFoundException(id) ;
+    }
+
+    public static void delete (int id) throws EntityNotFoundException{
+        Entity e = get(id) ;
+        entities.remove(e) ;
+    }
+
+    public static void update (Entity e) throws EntityNotFoundException {
+
+        Entity existingEntity = get(e.id) ;
+        Entity copy = e.copy() ;
+        entities.set(entities.indexOf(existingEntity) , copy) ;
+    }
+}
---
+++ src/example/Human.java
@@ -0,0 +1,19 @@
+package example;
+
+import db.Entity;
+
+public class Human extends Entity {
+    public String name ;
+
+    public Human (String name) {
+        this.name = name ;
+    }
+
+    @Override
+    public Human copy () {
+        Human copy = new Human(new String(this.name)) ;
+        copy.id = this.id ;
+        return copy ;
+    }
+
+}
