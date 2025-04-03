e5510752e137b47924b701e7754778cb5714663f
sara <sarashmsi834@gmail.com>
2025-04-03 11:10:07 +0330

    add validators

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 import db.Database;
 import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
 import example.Human;
+import example.HumanValidator;
 
 
 public class Main {
-    public static void main(String[] args) {
-        Human ali = new Human("Ali");
+    public static void main(String[] args) throws InvalidEntityException {
+        Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
+
+        Human ali = new Human("Ali", -10);
         Database.add(ali);
-
-        ali.name = "Ali Hosseini";
-
-        Human aliFromTheDatabase = (Human) Database.get(ali.id);
-
-        System.out.println("ali's name in the database: " + aliFromTheDatabase.name);
     }
 }
